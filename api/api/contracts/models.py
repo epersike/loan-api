9d833b28bee5b68a707739e66f1cb4f38e89aa19
Elton Persike <epersike@MacBook-Air-de-Elton.local>
2019-05-29 23:19:41 -0300

    Created initial models

--- api/api/contracts/models.py
+++ api/api/contracts/models.py
@@ -6,6 +6,19 @@ from django.db import models
 class Contract(models.Model):
 
     id = models.AutoField(primary_key=True, editable=False)
-    amount = models.DecimalField(decimal_places=2)
-    interest_rate = models.DecimalField(decimal_places=2)
+    total_amount = models.DecimalField(decimal_places=2, max_digits=10)
+    qty_payment = models.IntegerField()
+    interest_rate = models.DecimalField(decimal_places=2, max_digits=10)
     ip_address = models.CharField(max_length=32)
+    ts_subscription = models.DateField(auto_now=False, auto_now_add=True)
+    bank_name = models.CharField(max_length=50)
+    customer_name = models.CharField(max_length=50)
+
+class Payment(models.Model):
+    id = models.AutoField(primary_key=True, editable=False)
+    payment_original_date = models.DateField(auto_now=False, auto_now_add=False) # Customer should pay this 
+    payment_date = models.DateField(auto_now=False, auto_now_add=False)
+    subtotal = models.DecimalField(decimal_places=2, max_digits=10) # total_amount / qty_payment - The original amount customer should pay
+    total = models.DecimalField(decimal_places=2, max_digits=10) # subtotal + interest_rate - Total actually paid
+    contract = models.ForeignKey(to=Contract, on_delete=models.CASCADE)
+
